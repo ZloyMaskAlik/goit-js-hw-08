ed1d1792e4adbc3eca07aef805b4348ec0866e64
Alex Zloy <usvplus@gmail.com>
2024-11-29 20:50:37 +0200

    goit-js-hw-04

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,25 @@
-function filterArray(numbers, value) {
-    const filterResult = [];
-    for ( let i = 0 ; i < numbers.length ; i++ ) {
-        if (numbers[i] > value) {
-            filterResult.push(numbers[i]);
-        }
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours`;
+    },
+  
+    changeUsername(newName) {
+        this.username = newName;
+    },
+    
+    updatePlayTime(hours) {
+        this.playTime = hours;
     }
-   return filterResult
-}
+};
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(320);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
